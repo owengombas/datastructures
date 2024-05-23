96b90da98d4df97d6accab93e45a7526e6e5797c
owengombas <public.owen@gombas.ch>
2024-05-23 16:16:42 +0200

    Benchmarks

--- tests/benchmark_test.go
+++ tests/benchmark_test.go
@@ -1,97 +1,34 @@
 package tests
 
 import (
+	"fmt"
 	"testing"
 )
 
-func BenchmarkSequentialInserts1k(b *testing.B) {
-	keys := GenerateRandomKeys(1_000)
-	b.ResetTimer()
-	_, _, err := SequentialInserts(keys)
-	if err != nil {
-		b.Error(err)
-	}
+func BenchmarkSequentialInserts1kTo1m(b *testing.B) {
+	b.Run("SequentialInserts", func(b *testing.B) {
+		keys := GenerateRandomKeys(b.N)
+		b.ResetTimer()
+
+		_, _, err := SequentialInserts(keys)
+		if err != nil {
+			b.Error(err)
+		}
+	})
 }
 
-func BenchmarkSequentialInserts10k(b *testing.B) {
-	keys := GenerateRandomKeys(10_000)
-	b.ResetTimer()
-	_, _, err := SequentialInserts(keys)
-	if err != nil {
-		b.Error(err)
-	}
-}
+func BenchmarkSequentialLookups(b *testing.B) {
+	b.Run(fmt.Sprintf("SequentialLookups"), func(b *testing.B) {
+		keys := GenerateRandomKeys(b.N)
+		index, _, err := SequentialInserts(keys)
+		if err != nil {
+			b.Error(err)
+		}
 
-func BenchmarkSequentialInserts100k(b *testing.B) {
-	keys := GenerateRandomKeys(100_000)
-	b.ResetTimer()
-	_, _, err := SequentialInserts(keys)
-	if err != nil {
-		b.Error(err)
-	}
-}
-
-func BenchmarkSequentialInserts1m(b *testing.B) {
-	keys := GenerateRandomKeys(1_000_000)
-	b.ResetTimer()
-	_, _, err := SequentialInserts(keys)
-	if err != nil {
-		b.Error(err)
-	}
-}
-
-func BenchmarkSequentialLookups1k(b *testing.B) {
-	keys := GenerateRandomKeys(1_000)
-	index, _, err := SequentialInserts(keys)
-	if err != nil {
-		b.Error(err)
-	}
-
-	b.ResetTimer()
-	err = SequentialLookups(index, keys)
-	if err != nil {
-		b.Error(err)
-	}
-}
-
-func BenchmarkSequentialLookups10k(b *testing.B) {
-	keys := GenerateRandomKeys(10_000)
-	index, _, err := SequentialInserts(keys)
-	if err != nil {
-		b.Error(err)
-	}
-
-	b.ResetTimer()
-	err = SequentialLookups(index, keys)
-	if err != nil {
-		b.Error(err)
-	}
-}
-
-func BenchmarkSequentialLookups100k(b *testing.B) {
-	keys := GenerateRandomKeys(100_000)
-	index, _, err := SequentialInserts(keys)
-	if err != nil {
-		b.Error(err)
-	}
-
-	b.ResetTimer()
-	err = SequentialLookups(index, keys)
-	if err != nil {
-		b.Error(err)
-	}
-}
-
-func BenchmarkSequentialLookups1m(b *testing.B) {
-	keys := GenerateRandomKeys(1_000_000)
-	index, _, err := SequentialInserts(keys)
-	if err != nil {
-		b.Error(err)
-	}
-
-	b.ResetTimer()
-	err = SequentialLookups(index, keys)
-	if err != nil {
-		b.Error(err)
-	}
+		b.ResetTimer()
+		err = SequentialLookups(index, keys)
+		if err != nil {
+			b.Error(err)
+		}
+	})
 }
